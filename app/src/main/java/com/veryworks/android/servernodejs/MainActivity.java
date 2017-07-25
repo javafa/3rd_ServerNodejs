721b429695e17f8e03d25b38d940b7a8f6202d8d
javafa <nigne2016@gmail.com>
2017-07-25 13:17:20 +0900

    add retrofit responseBody

--- app/src/main/java/com/veryworks/android/servernodejs/MainActivity.java
+++ app/src/main/java/com/veryworks/android/servernodejs/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Button;
 
 import java.util.ArrayList;
@@ -12,9 +13,9 @@ import java.util.List;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new RecyclerAdapter(this, data);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        loader();
     }
 
     private void initView() {
@@ -48,22 +51,22 @@ public class MainActivity extends AppCompatActivity {
         // 1. 레트로핏 생성
         Retrofit client = new Retrofit.Builder()
                 .baseUrl(MyServer.SERVER)
-                .addConverterFactory(GsonConverterFactory.create())
+                //.addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
         // 2. 서비스 연결
         MyServer myServer = client.create(MyServer.class);
 
         // 3. 서비스의 특정 함수 호출 -> Observable 생성
-        Observable<List<Bbs>> observable = myServer.read();
+        Observable<ResponseBody> observable = myServer.read();
 
         // 4. subscribe 등록
         observable.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                data -> {
+                responseBody -> {
                     // 1. 데이터를 꺼내고
-
+                    Log.e("Retrofit","data="+responseBody.string());
                     // 2. 데이터를 아답터에 세팅하고
 
                     // 3. 아답터 갱신
@@ -75,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         public static final String SERVER = "http://192.168.10.240/";
 
         @GET("bbs")
-        public Observable<List<Bbs>> read();
+        public Observable<ResponseBody> read();
 
         @POST("bbs")
         public void write(Bbs bbs);
