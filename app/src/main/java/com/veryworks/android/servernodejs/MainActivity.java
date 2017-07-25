5b559a8e305ca36df77f65dded0f8bdd5c3ec01a
javafa <nigne2016@gmail.com>
2017-07-25 11:00:12 +0900

    lambda test

--- app/src/main/java/com/veryworks/android/servernodejs/MainActivity.java
+++ app/src/main/java/com/veryworks/android/servernodejs/MainActivity.java
@@ -1,7 +1,8 @@
 package com.veryworks.android.servernodejs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lambdaTest();
     }
 
     private void lambdaTest(){
-
+        new Thread(
+                ()->Log.i("Lambda","running============ok")
+        ).start();
     }
 }
